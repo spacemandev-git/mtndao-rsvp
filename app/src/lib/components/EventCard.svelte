1dc8635fec488f8ce1373397ba93e45889450a0c
jollygrin <JollyGrin@protonmail.com>
2025-02-13 12:30:27 -0700

    clean up event card

--- app/src/lib/components/EventCard.svelte
+++ app/src/lib/components/EventCard.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import IconPin from "./icons/IconPin.svelte";
+  import IconClock from "./icons/IconClock.svelte";
+  import IconCal from "./icons/IconCal.svelte";
+
   let {
     event,
   }: {
@@ -13,6 +17,7 @@
   } = $props();
 
   function handleRSVP(eventId: string, action: "rsvp" | "confirm") {}
+  const isCreator = true;
 </script>
 
 <div
@@ -24,57 +29,15 @@
     </h2>
     <div class="text-gray-600 mb-4 text-sm sm:text-base">
       <p class="flex items-center mb-1">
-        <svg
-          class="w-4 h-4 mr-2 flex-shrink-0"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
+        <IconCal />
         {event.date}
       </p>
       <p class="flex items-center mb-1">
-        <svg
-          class="w-4 h-4 mr-2 flex-shrink-0"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <IconClock />
         {event.time}
       </p>
       <p class="flex items-center">
-        <svg
-          class="w-4 h-4 mr-2 flex-shrink-0"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-          />
-        </svg>
+        <IconPin />
         <span class="break-words">{event.location}</span>
       </p>
     </div>
