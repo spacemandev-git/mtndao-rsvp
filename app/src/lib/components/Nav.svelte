559e8897e1823c4393509958b82285ffb0fc6072
spacemandev <devbharel@gmail.com>
2025-02-18 20:42:33 -0700

    fuck me

--- app/src/lib/components/Nav.svelte
+++ app/src/lib/components/Nav.svelte
@@ -1,48 +1,51 @@
 <script lang="ts">
-  import { walletStore } from "$lib/wallet/walletStore.svelte";
-  import {
-    connectWallet,
-    disconnectWallet,
-  } from "$lib/wallet/helpers/connect-wallet";
-  let {
-    onOpen,
-  }: {
-    onOpen?(): void;
-  } = $props();
+    import { walletStore } from "$lib/wallet/walletStore.svelte";
+    import {
+        connectWallet,
+        disconnectWallet,
+    } from "$lib/wallet/helpers/connect-wallet";
+    import toast from "svelte-french-toast";
+    let {
+        onOpen,
+    }: {
+        onOpen?(): void;
+    } = $props();
 
-  function truncateAddress(address: string) {
-    if (!address) return "";
-    return `${address.slice(0, 4)}...${address.slice(-4)}`;
-  }
+    function truncateAddress(address: string) {
+        if (!address) return "";
+        return `${address.slice(0, 4)}...${address.slice(-4)}`;
+    }
 </script>
 
 <nav class="bg-white shadow-lg">
-  <div class="container mx-auto px-4">
-    <div
-      class="flex flex-col sm:flex-row justify-between items-center py-4 sm:h-16 space-y-4 sm:space-y-0"
-    >
-      <div class="text-xl font-bold">RSVP dApp</div>
-      <div
-        class="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4 w-full sm:w-auto"
-      >
-        <button
-          onclick={onOpen}
-          class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md transition-colors duration-200 w-full sm:w-auto"
+    <div class="container mx-auto px-4">
+        <div
+            class="flex flex-col sm:flex-row justify-between items-center py-4 sm:h-16 space-y-4 sm:space-y-0"
         >
-          Create Event
-        </button>
-        <button
-          onclick={async () => {
-            if (!$walletStore.walletAddress) await connectWallet();
-            else await disconnectWallet();
-          }}
-          class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition-colors duration-200 w-full sm:w-auto text-sm sm:text-base"
-        >
-          {$walletStore.walletAddress
-            ? truncateAddress($walletStore.walletAddress)
-            : "Connect Wallet"}
-        </button>
-      </div>
+            <div class="text-xl font-bold">RSVP dApp</div>
+            <div
+                class="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4 w-full sm:w-auto"
+            >
+                <button
+                    onclick={onOpen}
+                    class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md transition-colors duration-200 w-full sm:w-auto"
+                >
+                    Create Event
+                </button>
+                <button
+                    onclick={async () => {
+                        if (!$walletStore.walletAddress) {
+                            toast.loading("Connecting wallet...");
+                            await connectWallet();
+                        } else await disconnectWallet();
+                    }}
+                    class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md transition-colors duration-200 w-full sm:w-auto text-sm sm:text-base"
+                >
+                    {$walletStore.walletAddress
+                        ? truncateAddress($walletStore.walletAddress)
+                        : "Connect Wallet"}
+                </button>
+            </div>
+        </div>
     </div>
-  </div>
 </nav>
--- app/src/lib/wallet/helpers/connect-wallet.ts
+++ app/src/lib/wallet/helpers/connect-wallet.ts
@@ -6,70 +6,69 @@ const LOCAL_STORAGE_KEY = "rsvp-walletaddress";
 
 // Check if Phantom wallet is available
 export const checkWallet = async () => {
-  if (window.solana || window.solflare || window.backpack) {
-    console.info("Wallet found!");
-    if (window.solana) {
-      return window.solana;
-    } else if (window.solflare) {
-      return window.solflare;
-    } else if (window.backpack) {
-      return window.backpack;
-    }
-  } else {
-    console.warn("Wallet not found. Please install it.");
-    toast.error("Wallet not found");
-
-    return null;
-  }
+	if (window.solana || window.solflare || window.backpack) {
+		console.info("Wallet found!");
+		if (window.solana) {
+			return window.solana;
+		} else if (window.solflare) {
+			return window.solflare;
+		} else if (window.backpack) {
+			return window.backpack;
+		}
+	} else {
+		console.warn("Wallet not found. Please install it.");
+		toast.error("Wallet not found");
+		
+		return null;
+	}
 };
 
 // Connect to the wallet
 export const connectWallet = async (connection?: Connection) => {
-  const wallet = await checkWallet();
-  if (!wallet) return;
-  try {
-    const response = await wallet.connect();
-    const walletAddress = response.publicKey.toString();
-
-    // TODO: replace this with api - rpc will be handled by backend
-    const balance = 1;
-
-    // update wallet store with wallet address and balance
-    walletStore.update((store) => ({
-      ...store,
-      walletAddress,
-      balance: 1,
-    }));
-
-    // store wallet address in local storage
-    localStorage.setItem(LOCAL_STORAGE_KEY, walletAddress);
-
-    return { walletAddress, balance };
-  } catch (error) {
-    console.error("Wallet connection failed:", error);
-  }
+	const wallet = await checkWallet();
+	if (!wallet) return;
+	try {
+		const response = await wallet.connect();
+		const walletAddress = response.publicKey.toString();
+		
+		// TODO: replace this with api - rpc will be handled by backend
+		const balance = 1;
+		
+		// update wallet store with wallet address and balance
+		walletStore.update((store) => ({
+			...store,
+			walletAddress,
+			balance: 1,
+		}));
+		
+		// store wallet address in local storage
+		localStorage.setItem(LOCAL_STORAGE_KEY, walletAddress);
+		
+		return { walletAddress, balance };
+	} catch (error) {
+		console.error("Wallet connection failed:", error);
+	}
 };
 
 export const disconnectWallet = async () => {
-  const wallet = await checkWallet();
-  if (!wallet) return;
-
-  try {
-    // Disconnect the wallet
-    await wallet.disconnect();
-
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
-
-    // Clear wallet information from the store
-    walletStore.update((store) => ({
-      ...store,
-      walletAddress: null,
-      balance: 0,
-    }));
-
-    // Optional: Clear other session data if needed
-    // For example, if you store token data or other wallet-related data in localStorage or sessionStorage
-  } catch (error) {
-    console.error("Wallet disconnection failed:", error);
-  }
+	const wallet = await checkWallet();
+	if (!wallet) return;
+	try {
+		// Disconnect the wallet
+		await wallet.disconnect();
+		
+		localStorage.removeItem(LOCAL_STORAGE_KEY);
+		
+		// Clear wallet information from the store
+		walletStore.update((store) => ({
+			...store,
+			walletAddress: null,
+			balance: 0,
+		}));
+		
+		// Optional: Clear other session data if needed
+		// For example, if you store token data or other wallet-related data in localStorage or sessionStorage
+	} catch (error) {
+		console.error("Wallet disconnection failed:", error);
+	}
 };
